5b25cdb22b58c6741bb88a50fd9cd86511a9a8f2
heather-hx <zhh1246@163.com>
2018-02-21 14:40:36 +0800

    commit

---
+++ js/js.js
@@ -0,0 +1,80 @@
+
+
+$('#scene').parallax();
+
+
+	var mySwiper = new Swiper ('.swiper-container', {
+	    direction: 'horizontal',
+	    loop: true,
+	    onlyExternal : true,
+	    nextButton: '.swiper-button-next',
+	    prevButton: '.swiper-button-prev',
+	    effect : 'coverflow',
+		slidesPerView: 1.5,
+		centeredSlides: true,
+		coverflow: {
+            rotate:10,
+            stretch:50,
+            depth:50,
+            modifier: 2,
+            slideShadows : true
+		}
+			    
+	}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
